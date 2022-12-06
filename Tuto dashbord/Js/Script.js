135f2c5ab425064a3b32a96790cee9b93579ed93
MarleyApong <108243674+MarleyApong@users.noreply.github.com>
2022-12-06 04:14:45 +0100

    Delete Tuto dashbord directory

--- Tuto dashbord/Js/Script.js
+++
@@ -1,8 +0,0 @@
-let Navigation = document.querySelector(".Navigation");
-let Main = document.querySelector(".Main");
-let Toggle = document.querySelector(".Toggle");
-
-Toggle.onclick = () => {
-    Navigation.classList.toggle("active");
-    Main.classList.toggle("active");
-}
